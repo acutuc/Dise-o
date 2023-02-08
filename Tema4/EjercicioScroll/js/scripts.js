7935cce672c21a3d0a7fb555bb635698dad304ad
acutuc <acutuc94@gmail.com>
2023-02-08 14:36:49 +0100

    Subida práctica 6

--- Tema4/EjercicioScroll/js/scripts.js
+++ Tema4/EjercicioScroll/js/scripts.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
-
-    $("input").focusin(function(e){
-        $(this).css("background-color", "lightgrey")
-    })
-
-    $("input").focusout(function(e){
-        $(this).css("background-color", "white")
-    })
-
+    $(window).scroll(function () {
+        $("div#contenido").css("padding-top", "62px")
+        $("div.navbar").css("position", "fixed");
+        $("div.navbar").css("background-color", "white");
+        $("div.navbar").css("opacity", "0.9");
+        $("div.navbar > div.container > div.menu-items").css("background-color", "white");
+    });
+    $(window).resize(function () { 
+        $("input.checkbox").prop("checked", false);
+    });
 });
