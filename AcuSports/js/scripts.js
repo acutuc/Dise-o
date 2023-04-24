4bd0c81ecf638982ab42f79b3267e6b49b01ff48
Gabriel Allende <acutuc94@gmail.com>
2023-04-24 18:36:26 +0200

    Terminar proyecto

--- AcuSports/js/scripts.js
+++ AcuSports/js/scripts.js
@@ -1,3 +1,4 @@
+/* Menú desplegable */
 const btnToggle = document.querySelector('.toggle-btn');
 
 btnToggle.addEventListener('click', function () {
@@ -7,6 +8,7 @@ btnToggle.addEventListener('click', function () {
 /* COOKIES */
 function checkAcceptCookies() {
     if (localStorage.acceptCookies == 'true') {
+        $('#div-cookies').show(); // <---------------------------------- Quitar esta línea. La dejo para que Lorite vea las cookies.
     } else {
         $('#div-cookies').show();
     }
@@ -18,3 +20,13 @@ function acceptCookies() {
 $(document).ready(function() {
     checkAcceptCookies();
 });
+
+/* Cambio de página a participantes.html al pulsar en un evento en concreto*/
+function eventos(){
+    document.location.href = "../src/participantes.html";
+}
+
+/* Alert al darle a iniciar sesión*/
+function alert(){
+    alert("INICIO DE SESIÓN")
+}
