0666e7f0515c1c230d0fb0293622f17dd5f3700f
acutuc <acutuc94@gmail.com>
2023-05-09 13:44:51 +0200

    Terminar menu lateral

--- AcuSports/js/scripts.js
+++ AcuSports/js/scripts.js
@@ -1,9 +1,37 @@
-/* Menú desplegable */
-const btnToggle = document.querySelector('.toggle-btn');
 
-btnToggle.addEventListener('click', function () {
-  document.getElementById('sidebar').classList.toggle('active');
-});
+window.onload = function () {
+    /* Menú desplegable */
+    $('div#sidebar').click(function () {
+        $('main#principal').animate({
+            'opacity': '0.2'
+        }, 500)
+        $(this).toggleClass('active')
+    })
+    if($('div#sidebar').hasClass('desactivado')){
+        $('main#principal').animate({
+            'opacity' : '1'
+        }, 500)
+    }
+
+    /* Header fijo al hacer scroll*/
+    $(this).scroll(function () {
+        $('header').addClass('menu_fijo');
+        if ($(this).scrollTop() == 0) {
+            $('header').removeClass('menu_fijo');
+        }
+    });
+
+}
+
+/* Cambio de página a participantes.html al pulsar en un evento en concreto*/
+function eventos() {
+    document.location.href = "../src/participantes.html";
+}
+
+/* Alert al darle a iniciar sesión*/
+function alert() {
+    alert("INICIO DE SESIÓN")
+}
 
 /* COOKIES */
 function checkAcceptCookies() {
@@ -17,16 +45,6 @@ function acceptCookies() {
     localStorage.acceptCookies = 'true';
     $('#div-cookies').hide();
 }
-$(document).ready(function() {
+$(document).ready(function () {
     checkAcceptCookies();
 });
-
-/* Cambio de página a participantes.html al pulsar en un evento en concreto*/
-function eventos(){
-    document.location.href = "../src/participantes.html";
-}
-
-/* Alert al darle a iniciar sesión*/
-function alert(){
-    alert("INICIO DE SESIÓN")
-}
