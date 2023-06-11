1da517b3c3cf55a400e6ba474593ca98bf25dc20
Gabriel Allende <acutuc94@gmail.com>
2023-06-11 18:01:22 +0200

    Continuar desde aquí

--- AcuSports/js/scripts.js
+++ AcuSports/js/scripts.js
@@ -229,22 +229,167 @@ function eventos() {
 }
 
 /* Alert al darle a iniciar sesión*/
-function alert() {
+function iniciarSesion(e) {
     alert("INICIO DE SESIÓN")
+    e.preventDefault()
 }
 
 /* COOKIES */
-function checkAceptarCookies() {
-    if (localStorage.aceptarCookies == 'true') {
-        $('#div-cookies').show(); // <---------------------------------- Quitar esta línea. La dejo para que Lorite vea las cookies.
+function checkacceptCookies() {
+    if (localStorage.acceptCookies == 'true') {
+        $('#div-cookies').show(); // <---------------------------------- Quitar esta línea. La dejo para que María José vea las cookies.
     } else {
         $('#div-cookies').show();
     }
 }
-function aceptarCookies() {
-    localStorage.aceptarCookies = 'true';
+function acceptCookies() {
+    localStorage.acceptCookies = 'true';
     $('#div-cookies').hide();
 }
 $(document).ready(function () {
-    checkAceptarCookies();
+    checkacceptCookies();
 });
+
+//REPRODUCTOR:
+window.onload = function () {
+    var video = document.getElementById('reproductor');
+    var tiempoTotal = document.getElementById('total');
+    var tiempoTrans = document.getElementById('trans');
+    var play = document.getElementById('play');
+    var stop = document.getElementById('stop');
+    var restart = document.getElementById('restart');
+    var mute = document.getElementById('mute');
+    var subirVol = document.getElementById('subirVol');
+    var bajarVol = document.getElementById('bajarVol');
+    var masVel = document.getElementById('aumentarVel');
+    var menosVel = document.getElementById('disminuirVel');
+    var saltar = document.getElementById('saltoAdelante');
+    var retroceder = document.getElementById('saltoAtras');
+    var barraTiempo = document.getElementById('barraTiempo');
+    var pantalla = document.getElementById('screen');
+  
+    video.addEventListener('volumechange', function () {
+      volumenVideo = video.volume;
+    });
+  
+    barraTiempo.max = video.duration;
+    barraTiempo.value = 0;
+    if (video.duration > 60) {
+      tiempoTotal.innerHTML = parseInt(video.duration / 60) + ":" + parseInt(video.duration % 60);
+    } else {
+      if(isNaN(video.duration)){
+        tiempoTotal.innerHTML = "0:33";
+      }else{
+        tiempoTotal.innerHTML = "0:" + parseInt(video.duration);
+      }    
+    }
+  
+    video.addEventListener('timeupdate', function () {
+      barraTiempo.value = this.currentTime;
+      var minutos;
+      var segundos;
+      if (video.currentTime >= 60) {
+        minutos = parseInt(video.currentTime / 60);
+        segundos = parseInt(video.currentTime % 60);
+      } else {
+        segundos = parseInt(video.currentTime);
+        minutos = 0;
+      }
+      if ((segundos / 10) >= 1) {
+        tiempoTrans.innerHTML = minutos + ":" + segundos;
+      } else {
+        tiempoTrans.innerHTML = minutos + ":0" + segundos;
+      }
+    });
+  
+    play.addEventListener('click', function () {
+      if (play.value == '0') {
+        video.play();
+        play.value = '1';
+        play.firstChild.src = "../images/pause.svg";
+      } else {
+        video.pause();
+        play.value = '0';
+        play.firstChild.src = "../images/play.svg";
+      }
+    })
+  
+    stop.addEventListener('click', function () {
+      video.pause();
+      video.currentTime = 0;
+      play.value = '0';
+      play.firstChild.src = "../images/play.svg";
+    })
+  
+    restart.addEventListener('click', function () {
+      video.currentTime = 0;
+    });
+  
+    mute.addEventListener('click', function () {
+      if (video.volume < 0.1) {
+        video.volume = 0.5;
+        document.getElementById('altavoz').src = "../images/altavoz.svg";
+      } else {
+        video.volume = 0;
+        document.getElementById('altavoz').src = "../images/mute.svg";
+      }
+    })
+  
+    subirVol.addEventListener('click', function () {
+      if (video.volume < 1.0) {
+        video.volume += 0.1;
+      }
+      document.getElementById('altavoz').src = "../images/altavoz.svg";
+      volumenVideo = video.volume;
+    });
+  
+    bajarVol.addEventListener('click', function () {
+      if (video.volume > 0.1) {
+        video.volume -= 0.1;
+      } else {
+        document.getElementById('altavoz').src = "../images/mute.svg";
+      }
+      volumenVideo = video.volume;
+    });
+  
+    masVel.addEventListener('click', function () {
+      if (video.playbackRate < 2) {
+        video.playbackRate += 0.1;
+      }
+      document.getElementById('velocidad').innerHTML = video.playbackRate.toFixed(1);
+    });
+  
+    menosVel.addEventListener('click', function () {
+      if (video.playbackRate > 0.1) {
+        video.playbackRate -= 0.1;
+      }
+      document.getElementById('velocidad').innerHTML = video.playbackRate.toFixed(1);
+    });
+  
+    saltar.addEventListener('click', function () {
+      if (video.currentTime < (video.duration - video.duration / 10)) {
+        video.currentTime += parseInt((video.duration / 10));
+      }
+    });
+  
+    retroceder.addEventListener('click', function () {
+      if (video.currentTime > (video.duration / 10)) {
+        video.currentTime -= parseInt((video.duration / 10));
+      }
+    });
+  
+    barraTiempo.addEventListener('input', function () {
+      video.currentTime = this.value;
+      video.pause();
+    });
+  
+    barraTiempo.addEventListener('change', function () {
+      video.play();
+    });
+  
+    pantalla.addEventListener("click", function () {
+      video.requestFullscreen();
+    });
+  
+  };
+  
