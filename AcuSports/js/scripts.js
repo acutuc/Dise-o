36176df62ef1f0d0ca8808f711045408a1cc8135
acutuc <acutuc94@gmail.com>
2023-06-11 05:26:22 +0200

    Terminar

--- AcuSports/js/scripts.js
+++ AcuSports/js/scripts.js
@@ -37,8 +37,10 @@ $(document).ready(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 0) {
             $('header').addClass('header-fijo');
+            $("main").css({ "padding-top": "75px" })
         } else {
             $('header').removeClass('header-fijo');
+            $("main").css({ "padding-top": "0px" })
         }
     });
 
@@ -108,6 +110,117 @@ $(document).ready(function () {
     }
     );
 
+    // Volver arriba:
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $("#go-up").fadeIn(500);
+            $("header").css({
+                "position": "fixed",
+                "width": "100%",
+                "opacity": "0.9"
+            })
+        } else {
+            $("#go-up").fadeOut(500)
+            $("header").removeAttr("style")
+        }
+    });
+
+    $("#go-up").click(function (e) {
+        e.preventDefault();
+
+        $("html, body").stop(true).animate({
+            scrollTop: 0
+        }, "swing")
+    });
+
+    //Ver perfil en participantes:
+    $("span.circulo").hover(function () {
+        // over
+        $(this).css({
+            "background-color": "#1f1f1f",
+            "opacity" : "0.5"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "background-color": "#D9D9D9"
+        }).ap
+    })
+
+    //Iniciar sesión en participantes:
+    $("a#iniciar").hover(function () {
+        // over
+        $(this).css({
+            "background-color": "#14570b"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "background-color": "#3B9E2D"
+        }).ap
+    })
+
+    //Menú lateral
+    $("ul > a > li").hover(function () {
+        // over
+        $(this).css({
+            "background-color": "#14570b"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "background-color": "#787575"
+        })
+    })
+
+    //RRSS Footer:
+    $("i.fa-instagram").hover(function () {
+        // over
+        $(this).css({
+            "color": "pink"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "color": "white"
+        })
+    })
+
+    $("i.fa-facebook").hover(function () {
+        // over
+        $(this).css({
+            "color": "blue"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "color": "white"
+        })
+    })
+
+    $("i.fa-whatsapp").hover(function () {
+        // over
+        $(this).css({
+            "color": "green"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "color": "white"
+        })
+    })
+
+    $("i.fa-twitter").hover(function () {
+        // over
+        $(this).css({
+            "color": "lightblue"
+        })
+    }, function () {
+        // out
+        $(this).css({
+            "color": "white"
+        })
+    })
 });
 
 /* Cambio de página a participantes.html al pulsar en un evento en concreto*/
@@ -121,17 +234,17 @@ function alert() {
 }
 
 /* COOKIES */
-function checkAcceptCookies() {
-    if (localStorage.acceptCookies == 'true') {
+function checkAceptarCookies() {
+    if (localStorage.aceptarCookies == 'true') {
         $('#div-cookies').show(); // <---------------------------------- Quitar esta línea. La dejo para que Lorite vea las cookies.
     } else {
         $('#div-cookies').show();
     }
 }
-function acceptCookies() {
-    localStorage.acceptCookies = 'true';
+function aceptarCookies() {
+    localStorage.aceptarCookies = 'true';
     $('#div-cookies').hide();
 }
 $(document).ready(function () {
-    checkAcceptCookies();
+    checkAceptarCookies();
 });
